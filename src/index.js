1e8efc79d522114ec920453f543dcf51c0c63019
Hyunjoon Kim <joonkim0625@gmail.com>
2018-11-05 15:38:50 +0900

    implement login request

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import "@babel/polyfill"; // 이 라인을 지우지 말아주세요!
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: "https://furtive-cornet.glitch.me/"
+});
 
 const templates = {
   loginForm: document.querySelector('#login-form').content
@@ -17,8 +22,21 @@ function drawLoginForm() {
   const loginFormEl = fragment.querySelector('.login-form')
   // 로그인 과정에서 통신을 할 것이니 async를 사용
   loginFormEl.addEventListener('submit', async e => {
-    alert('로그인 요청 전송')
+
     e.preventDefault()
+    // e: 이벤트 객체
+    // e.target: 이벤트를 실제로 일으킨 요소 객체(여기서는 loginFormEl)
+    // e.target.elements: 폼 내부에 들어있는 요소 객체를 편하게 가져올 수 있는 특이한 객체
+    // e.target.elements.username: name 어트리뷰트에 username이라고 지정되어있는 input 요소 객체
+    // .value: 사용자가 input 태그에 입력한 값
+    const username = e.target.elements.username.value
+    const password = e.target.elements.password.value
+
+    const res = await api.post('/users/login', {
+      username,
+      password
+    })
+    alert(res.data.token)
   })
   // 3. 문서 내부에 삽입하기
   rootEl.appendChild(fragment)
